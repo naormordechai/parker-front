84cf8e10c83b0cd38d954f7afa2f71f1f0bbf86e
rbrook <ranibrook@gmail.com>
2018-07-27 19:30:28 +0200

    merging

--- src/stores/store.js
+++ src/stores/store.js
@@ -8,16 +8,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-<<<<<<< HEAD
-    user :{
-      // _id : '5b583081f6d632e56ebd6a43',
-      firstName : '',
-      lastName: '',
-      eMail: ''
-    }
-=======
    
->>>>>>> 019c07706d432bb046a9613aac7c15a4d68f9f7a
   },
   getters: {
   },
