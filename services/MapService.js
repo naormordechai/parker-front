c9c136c6fa1189e4ac87c110fe5243bde656f984
rbrook <ranibrook@gmail.com>
2018-08-13 15:41:23 +0200

    merging new google API key

--- services/MapService.js
+++ services/MapService.js
@@ -27,11 +27,10 @@ function addMarker(loc) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = 'AIzaSyC6AifY9XewU3b2wEp0Dr7XKyOjSS1oebQ';
+    const API_KEY = 'AIzaSyDXtbBnFfIsE_Zn-6D4r2RtCrjk-9lvxvs';
    
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
-    // elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`;
 
     elGoogleApi.async = true;
     document.body.append(elGoogleApi);
@@ -51,11 +50,9 @@ function centerMap(lat,lng) {
 
 function loadLocation(value) {
 
-    const API_KEY = 'AIzaSyC6AifY9XewU3b2wEp0Dr7XKyOjSS1oebQ';
-    console.log(value)
+    const API_KEY = 'AIzaSyDXtbBnFfIsE_Zn-6D4r2RtCrjk-9lvxvs';
     return  axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${value}&key=${API_KEY}`)
     .then(function(res){
-        console.log('the data from searching an address', res.data)
         return res.data
     })
 } 
